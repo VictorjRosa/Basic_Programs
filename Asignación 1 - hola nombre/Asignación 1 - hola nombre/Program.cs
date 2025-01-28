5312a9c819eb8f8b51ff0b3d621585b9a76ad13e
VictorjRosa <victorjrosa98@gmail.com>
2025-01-28 11:44:46 -0400

    Add files via upload

---
+++ Asignación 1 - hola nombre/Asignación 1 - hola nombre/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asignación_1___hola_nombre
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string matricula = "19-1028";
+            string nombre = "Víctor José Rosa Depratt.";
+
+            Console.WriteLine("Asignación 1 - hola nombre\n\n");
+            Console.WriteLine("Hola " + matricula + " " + nombre);
+
+            Console.Read();
+        }
+    }
+}
