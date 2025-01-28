2c3f046908677a135eb371aa149841423de68b86
VictorjRosa <victorjrosa98@gmail.com>
2025-01-28 11:45:46 -0400

    Add files via upload

---
+++ Asignación 2 - Animales/Asignación 2 - Animales/Perro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asignación_2___Animales
+{
+    internal class Perro
+    {
+        public string Raza { get; set; }
+        public int Cant { get; set; }
+
+        public Perro(string raza, int cant)
+        {
+            Raza = raza;
+            Cant = cant;
+        }
+    }
+}
---
+++ Asignación 2 - Animales/Asignación 2 - Animales/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asignación_2___Animales
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite la cantidad de perros a introducir: ");
+            Perro[] perro = new Perro[Convert.ToInt16(Console.ReadLine())];
+            
+            for (int i = 0; i < perro.Length; i++)
+            {
+                Console.WriteLine("Digite raza del perro " + (i + 1) + ": ");
+                string raza = Console.ReadLine();
+                perro[i] = new Perro(raza, 1);
+            }
+            Console.Clear();
+
+            foreach (var Perro in perro)
+            {
+                Console.WriteLine($"Raza: {Perro.Raza}, cantidad: {Perro.Cant}");
+            }
+
+            Console.Read();
+        }
+    }
+}
