da7e117183541273c84a13cc39392a05d013f39e
VictorjRosa <victorjrosa98@gmail.com>
2025-02-09 11:31:41 -0400

    Add files via upload

---
+++ Tarea 3 - Udemy/Tarea 3 - Udemy/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace Tarea_3___Udemy
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
