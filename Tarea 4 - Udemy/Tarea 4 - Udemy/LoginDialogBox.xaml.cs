cb1c1b3e7675beae784c445da0369751e0871b43
VictorjRosa <victorjrosa98@gmail.com>
2025-02-09 11:33:06 -0400

    Add files via upload

---
+++ Tarea 4 - Udemy/Tarea 4 - Udemy/LoginDialogBox.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace Tarea_4___Udemy
+{
+    /// <summary>
+    /// Interaction logic for LoginDialogBox.xaml
+    /// </summary>
+    public partial class LoginDialogBox : Window
+    {
+        public LoginDialogBox()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+    }
+}
---
+++ Tarea 4 - Udemy/Tarea 4 - Udemy/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+
+namespace Tarea_4___Udemy
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+            LoginDialogBox login = new LoginDialogBox();
+            bool? result = login.ShowDialog();
+            if (result == true)
+            {
+                MessageBox.Show("Excelente, adelante!!!");
+                this.WindowState = WindowState.Normal;
+            }
+            else
+            {
+                MessageBox.Show("Lo lamento, adios!!!");
+                this.Close();
+            }
+        }
+    }
+}
