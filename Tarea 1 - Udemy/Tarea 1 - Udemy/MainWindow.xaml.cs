bccd1d438b6363cfed9051501d292b12264ffc1c
VictorjRosa <victorjrosa98@gmail.com>
2025-02-09 11:29:19 -0400

    Add files via upload

---
+++ Tarea 1 - Udemy/Tarea 1 - Udemy/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Tarea_1___Udemy
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void BtnAswer_Click(object sender, RoutedEventArgs e)
+        {
+            this.Cursor = Cursors.Wait;
+            System.Threading.Thread.Sleep(TimeSpan.FromSeconds(3));
+            Random random = new Random();
+            if (txtQuestion.Text == "")
+            {
+                MessageBox.Show("Pregunta algo antes.");
+            }
+            else
+            {
+                txtAnswer.Text = random.Next(0, 2) == 0 ? "No" : "Si";
+            }
+            this.Cursor = null;
+        }
+    }
+}
