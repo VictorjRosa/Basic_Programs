8017d1733cc488b39e5409b823f2ab084316ea88
VictorjRosa <victorjrosa98@gmail.com>
2025-02-09 11:30:09 -0400

    Add files via upload

---
+++ Tarea 2 - Udemy/Tarea 2 - Udemy/MyPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace Tarea_2___Udemy
+{
+    /// <summary>
+    /// Interaction logic for MyPage.xaml
+    /// </summary>
+    public partial class MyPage : Page
+    {
+        public MyPage()
+        {
+            InitializeComponent();
+        }
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            lblTitle.Text = "Mi Título";
+            lblContent.Text = "Mi contenido";
+        }
+
+        private void BtnBoton_Click(object sender, RoutedEventArgs e)
+        {
+            (this.Parent as NavigationWindow).Source = new Uri("https://www.google.com");
+        }
+    }
+}
